758b9164a094d1b27dde70e4e64fa84c38671b57
JGen1 <jengenes01@gmail.com>
2020-03-31 20:21:49 -0400

    Renamed routes.js (only using one route)

---
+++ routes/routes.js
@@ -0,0 +1,72 @@
+// getting express routers
+const router = require("express").Router();
+const mongojs = require("mongojs")
+const path = require("path")
+
+const db = require("../models")
+const Workout = require("../models/Workout.js");
+
+router.get('/exercise', function (req, res) {
+  res.sendfile(path.join(__dirname, '../public/exercise.html'))
+})
+router.get('/stats', function (req, res) {
+  res.sendfile(path.join(__dirname, '../public/stats.html'))
+})
+
+router.post("/api/workouts", ({ body }, res) => {
+  console.log("POST SUCCESSFUL")
+  Workout.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    })
+})
+
+// UPDATE A WORKOUT
+router.put("/api/workouts/:id", (req, res) => {
+  console.log("POST all data SUCCESSFUL")
+  let body = req.body;
+  console.log(body)
+
+  Workout.update(
+    { _id: mongojs.ObjectId(req.params.id) },
+    {
+      $push: {
+        exercises: {
+          type: body.type,
+          name: body.name,
+          duration: body.duration,
+          weight: body.weight,
+          reps: body.reps,
+          sets: body.sets,
+          distance: body.distance
+        }
+      }
+    })
+    .then(dbWorkout => {
+      res.json(dbWorkout)
+    })
+    .catch(err => {
+      res.status(400).json(err)
+    })
+})
+
+router.get("/api/workouts", (req, res) => {
+  db.Workout.find({}).then(dbCardio => {
+    res.json(dbCardio)
+  }).catch(err => {
+    res.json(err)
+  })
+})
+
+router.get("/api/workouts/range", (req, res) => {
+  db.Workout.find({}).then(dbCardio => {
+    res.json(dbCardio)
+  }).catch(err => {
+    res.json(err)
+  })
+})
+
+module.exports = router
